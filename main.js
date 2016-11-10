fcb35995cb1baef42c014a5a7a212daa89118b3d
shivam gulati <shivamgulati1991@gmail.com>
2016-11-09 19:06:45 -0500

    test

---
+++ main.js
@@ -0,0 +1,121 @@
+var redis = require('redis')
+var multer  = require('multer')
+var express = require('express')
+var fs      = require('fs')
+var app = express()
+// REDIS
+
+var startPort=parseInt(process.argv[2])
+var redis = require('redis')
+var client = redis.createClient(6379, '127.0.0.1', {})
+
+///////////// WEB ROUTES
+
+// Add hook to make it easier to get all visited URLS.
+app.use(function(req, res, next) 
+{
+	console.log(req.method, req.url);
+
+	// ... INSERT HERE.
+		client.lpush("recenturl", req.url, function(err, reply) {
+        client.ltrim("recenturl", 0, 9);
+    });
+
+	next(); // Passing the request to the next handler in the stack.
+});
+
+// PART 3: upload and meow
+app.post('/upload',[ multer({ dest: './uploads/'}), function(req, res){
+   if( req.files.image )
+   {
+	   fs.readFile( req.files.image.path, function (err, data) {
+	  		if (err) throw err;
+	  		var img = new Buffer(data).toString('base64');
+	  		
+	  		client.rpush('images',img);
+		});
+	}
+
+   res.status(204).end()
+ }]);
+
+app.get('/meow', function(req, res) {
+ 	{	
+	  	res.writeHead(200, {'content-type':'text/html'});
+ 		//if (err) throw err
+		client.rpop("images",function (err,imagedata){
+			if (err) throw err;
+		
+			if (imagedata)
+			{	
+			
+ 		        res.write("<h1>\n<img src='data:my_pic.jpg;base64,"+imagedata+"'/>");
+			}
+			res.end();
+ 		})
+ 	}
+ })
+
+// PART1: get/set methods
+app.get('/set', function(req, res) {
+    client.set("mykey", "this message will self-destruct in 10 seconds")
+    client.expire("mykey", 10)
+    res.send("mykey has been set")
+})
+
+app.get('/get', function(req, res) {
+    client.get("mykey", function(err, value) {
+        res.send(value)
+    });
+})
+
+// PART2: recent method
+app.get('/recent', function(req, res) {
+    client.lrange("recenturl", 0, 4, function(err, urls) {
+        res.send("Recently visited: "+ urls);
+    });
+});
+
+// PART24: spawn/destroy/listservers method
+app.get('/spawn', function(req, res) {
+	startPort=startPort+1
+var server = app.listen(startPort, function () 
+ {
+   var host = server.address().address
+   var port = server.address().port
+   client.lpush("serverlist",startPort) //push the port to server list
+   console.log('Now listening at http://%s:%s', host, port)
+});
+	res.send("New server: "+startPort)
+});
+
+app.get('/listservers', function(req, res) {
+    client.lrange("serverlist", -100, 100, function(err, value) {
+        res.send("Servers available: "+value)
+    });
+})
+
+app.get('/destroy', function(req, res) {   
+    client.LLEN("serverlist", function(err, value) {       
+        console.log("Length: "+ value)
+        //generate a random number
+        var rand = Math.floor((Math.random() * value) + 0);
+        //send the index to console
+        console.log(rand)
+        client.lindex('serverlist', rand, function(err, index){
+        client.lrem('serverlist', 1, index);    //remove the index value from list
+});
+   res.send("A server has been killed.");
+    });      
+    });
+
+// HTTP SERVER
+ var server = app.listen(process.argv[2], function () 
+ {
+
+   var host = server.address().address
+   var port = server.address().port
+   client.lpush("serverlist",port)
+   console.log('Example app listening at http://%s:%s', host, port)
+
+})
