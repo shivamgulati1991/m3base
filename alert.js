dcaff15623170b6fc5916dfb676657d882d29f00
shivam gulati <shivamgulati1991@gmail.com>
2016-11-09 22:33:54 -0500

    add alerts

--- alert.js
+++ alert.js
@@ -47,7 +47,7 @@ setInterval( function ()
     //client.set("proxy_flag", 0);
   } 
 
-  if (cpuLoad > 10) {
+  if (cpuLoad > 0) {
     //console.log("ALERT! CPU overload. SMS notification sent");
     twiClient.messages.create({
         from:+2024369391,
