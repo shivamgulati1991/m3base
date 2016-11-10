af11b6c623aa4a46c283cefed0306abfc59f40e7
shivam gulati <shivamgulati1991@gmail.com>
2016-11-10 14:56:14 -0500

    test proxy

--- proxy.js
+++ proxy.js
@@ -1,34 +1,56 @@
-var http = require('http');
-var httpProxy = require('http-proxy');
-var redis = require('redis');
+http = require('http');
+httpProxy = require('http-proxy');
+var express = require('express');
+var sioc = require('socket.io-client');
+var os = require('os');
 
-var client = redis.createClient(6379, '52.34.15.28', {}) 
+var interfaces = os.networkInterfaces();
 
 var proxy = httpProxy.createProxyServer({});
 
-var flag = 0;
+var canary_fail = false;
+
+var i = 0;
+
+var ipStable = "67.205.141.247";
+var ipCanary = "159.203.183.176";
+
+var socket = sioc('http://' + ipCanary + ':4006');
+
+// var socket = sioc('http://localhost:4006');
+
+socket.on("heartbeat", function(client) {
+    canary_fail = client.status;
+    if ((canary_fail == true) && (i == 0)) {
+        i++;
+        console.log("Canary Server Failed.");
+    }
+});
 
 var server = http.createServer(function(req, res) {
-	client.get("proxy_flag", function(err,flag_value){
-		if(flag_value == 1) {	
-			if (flag < 2) {
-				flag++;
-				proxy.web(req, res, {target: 'http://67.205.141.247:3000'}, function(err, data) {
-					console.log("Fetching request from production");
-				});
-			} else if (flag == 2) {
-				flag = 0;
-				proxy.web(req, res, {target: 'http://159.203.183.176:3000'}, function(err, data) {
-					console.log("Fetching request from CANARY");
-				});
-			}
-		} else {
-			proxy.web(req, res, {target: 'http://67.205.141.247:3000'}, function(err, data) {
-				console.log("Fetching request from production");
-			});
-		}
-	});
+
+    var port = 3000;
+    var tar = "http://" + ipStable + ":3000"; 
+    var percent = Math.floor((Math.random() * 10) + 1);
+    console.log("Number: " + percent);
+
+    if ((percent > 7) && (!canary_fail)) {
+      	port = 3001;
+        tar = "http://" + ipCanary + ":3000"; 
+        console.log("Forwarding request to [CANARY - " + ipCanary + ":" + port + "].");
+
+    } else {
+        port = 3000;
+        target = "http://" + ipStable + ":3000"; 
+        console.log("Forwarding request to [STABLE - " + ipStable + ":" + port + "].");
+    
+    }
+
+    proxy.web(req, res, {
+        target: tar
+    });
 
 });
 
-server.listen(8080);
+console.log("listening on port 5050")
+server.listen(5050);
